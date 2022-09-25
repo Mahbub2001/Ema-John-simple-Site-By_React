672acc1bcab7d95a37f7bd99dd225aef79545201
Mahbub2001 <imahbubturza@gmail.com>
2022-09-25 21:52:59 +0600

    added a new cart component

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -20,8 +21,8 @@ const Shop = () => {
         // console.log(product);
         const newCart = [...cart,product];
         SetCart(newCart);
+        
     }
-
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -35,8 +36,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selected Items : {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
